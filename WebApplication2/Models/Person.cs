96c71d4229ce9b38247a03e2a8e728e227a57dd9
Bhrugen Patel <bhrugen1990@gmail.com>
2024-04-10 10:04:48 -0500

    start

---
+++ WebApplication2/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApplication2.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+       public string Name { get; set; }
+        public int Age { get; set; }
+            public int Birthday { get; set; }
+        public int City { get; set; }
+        public int State { get; set; }
+        public int Country { get; set; }
+        public int ZipCode { get; set; }
+        public int PhoneNumber { get; set; }
+        public int Email { get; set; }
+        public int Password { get; set; }
+        public int ConfirmPassword { get; set; }
+
+    }
+    
+}
