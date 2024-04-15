763d49b9cb323dc2bc6b517ceb20ac256b8b74e4
Bhrugen Patel <bhrugen1990@gmail.com>
2024-04-15 16:27:35 -0500

    automatically based on name VS suggest code

--- WebApplication2/Service/ProductRepository.cs
+++ WebApplication2/Service/ProductRepository.cs
@@ -1,7 +1,45 @@
-﻿namespace WebApplication2.Service
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data;
+using WebApplication2.Models;
+
+namespace WebApplication2.Service
 {
     public class ProductRepository
     {
+        private readonly ApplicationDbContext _context;
 
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _context.Product.ToListAsync();
+        }
+
+        public async Task<Product> GetProduct(int id)
+        {
+            return await _context.Product.FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public async Task CreateProduct(Product product)
+        {
+            _context.Add(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateProduct(Product product)
+        {
+            _context.Update(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteProduct(int id)
+        {
+            var product = await _context.Product.FindAsync(id);
+            _context.Product.Remove(product);
+            await _context.SaveChangesAsync();
+        }
     }
 }
