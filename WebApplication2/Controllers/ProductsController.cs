20b42b23e3b782b3582ff697f07669e6ef82c2d8
Bhrugen Patel <bhrugen1990@gmail.com>
2024-04-15 16:31:53 -0500

    replace repository

--- WebApplication2/Controllers/ProductsController.cs
+++ WebApplication2/Controllers/ProductsController.cs
@@ -7,22 +7,24 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WebApplication2.Data;
 using WebApplication2.Models;
+using WebApplication2.Service;
 
 namespace WebApplication2.Controllers
 {
     public class ProductsController : Controller
     {
+        //use   in the controller rather than applicationdbcontext
         private readonly ApplicationDbContext _context;
+        private readonly ProductRepository _productRepository;
 
-        public ProductsController(ApplicationDbContext context)
+        public ProductsController(ProductRepository productRepository)
         {
-            _context = context;
+            _productRepository = productRepository;
         }
 
-        // GET: Products
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Product.ToListAsync());
+            return View(await _productRepository.GetProducts());
         }
 
         // GET: Products/Details/5
@@ -33,8 +35,7 @@ namespace WebApplication2.Controllers
                 return NotFound();
             }
 
-            var product = await _context.Product
-                .FirstOrDefaultAsync(m => m.Id == id);
+            var product = await _productRepository.GetProduct(id.Value);
             if (product == null)
             {
                 return NotFound();
@@ -43,47 +44,20 @@ namespace WebApplication2.Controllers
             return View(product);
         }
 
-        // GET: Products/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
         // POST: Products/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Name,Price,Category,Color")] Product product)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(product);
-                await _context.SaveChangesAsync();
+                await _productRepository.CreateProduct(product);
                 return RedirectToAction(nameof(Index));
             }
             return View(product);
         }
 
-        // GET: Products/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var product = await _context.Product.FindAsync(id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            return View(product);
-        }
-
         // POST: Products/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Price,Category,Color")] Product product)
@@ -97,8 +71,7 @@ namespace WebApplication2.Controllers
             {
                 try
                 {
-                    _context.Update(product);
-                    await _context.SaveChangesAsync();
+                    await _productRepository.UpdateProduct(product);
                 }
                 catch (DbUpdateConcurrencyException)
                 {
@@ -116,68 +89,34 @@ namespace WebApplication2.Controllers
             return View(product);
         }
 
-        // GET: Products/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var product = await _context.Product
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-
-            return View(product);
-        }
-
         // POST: Products/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var product = await _context.Product.FindAsync(id);
-            if (product != null)
-            {
-                _context.Product.Remove(product);
-            }
-
-            await _context.SaveChangesAsync();
+            await _productRepository.DeleteProduct(id);
             return RedirectToAction(nameof(Index));
         }
 
         private bool ProductExists(int id)
         {
-            return _context.Product.Any(e => e.Id == id);
+            return _productRepository.GetProduct(id) != null;
         }
 
         //create an endpoint GetProducts to retrieve all products
         [HttpGet]
         public IActionResult GetProducts()
         {
-            var products = _context.Product.ToListAsync().GetAwaiter().GetResult();
-            return Json(new { data =  products });
+            var products = _productRepository.GetProducts().Result;
+            return Json(new { data = products });
         }
 
         //  create endpoint in products controller to delete product by id
-        //  endpoint not invoked Failed to load resource: the server responded with a status of 500 () Products/Delete/2:1
         [HttpDelete("DeleteProduct/{id}")]
         public async Task<IActionResult> DeleteProduct(int id)
         {
-            var product = await _context.Product.FindAsync(id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-
-            _context.Product.Remove(product);
-            await _context.SaveChangesAsync();
-
+            await _productRepository.DeleteProduct(id);
             return NoContent();
         }
-        //select and wrtie this endpoint is never invoked
     }
 }
